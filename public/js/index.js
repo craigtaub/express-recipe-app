51080c5bce5dd70f802b49c1cfc7a9e3dd97bfb5
craigtaub <craig.taub@bbc.co.uk>
2015-08-22 10:54:28 +0100

    display error if no search results

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,38 @@
-(function(){
-  function Index () {
+(function (){
+  function Index() {
       methods = {
-          attach: function () {
-            var $rows = $('.table tr');
-            $('#search').keyup(function() {
-              var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+          showEmptyMessage($table) {
+              $table.html(function () {
+                  $('.empty-msg').css("display", "none");
+                  if ($('.table tr:visible').length === 0) {
+                      $('.empty-msg').css("display", "block");
+                  }
+              });
+          },
+          hideUnmatchedRows($rows, context) {
+              var val = $.trim($(context).val()).replace(/ +/g, ' ').toLowerCase();
 
-              $rows.show().filter(function() {
-                var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-                return text.indexOf(val) === -1;
-              }).hide();
-            });
+              $rows
+                  .show()
+                  .filter(function () {
+                      var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+                      return text.indexOf(val) === -1;
+                  })
+                  .hide();
+          },
+          attach: function () {
+              var $rows = $('.table tr'),
+                  $table = $('.table');
+
+              $('#search').keyup(function () {
+                  methods.hideUnmatchedRows($rows, this);
+                  methods.showEmptyMessage($table);
+              });
           }
       };
       return {
           init: function () {
-              setTimeout(methods.attach, 1);
-              // next browser tick
+              setTimeout(methods.attach, 1);  // next browser tick
           }
       };
   }
